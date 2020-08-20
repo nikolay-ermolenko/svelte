1e11f6b8c2701f8b55782a22962a456706419bb7
n.ermolenko <n.ermolenko@rt-solar.ru>
2020-08-20 14:35:59 +0300

    app: add checkboxes

--- src/App.svelte
+++ src/App.svelte
@@ -9,8 +9,23 @@
   export let version;
 
   let btnDisabled = false;
+  let checkNull = null;
+  let checkOptions = [{
+    id: '001',
+    name: 'Value A'
+  },{
+    id: '002',
+    name: 'Value B'
+  }];
+  let checkResult = ['001'];
+  let checkOptions2 = {
+    varA: true,
+    varB: false,
+    varC: null
+  };
 
   $: versionText = `svelte v${version}`;
+
 
   // header="Hello Svelte :: {version} :: qweertyyiuo"
 </script>
@@ -29,9 +44,16 @@
         <p class="version">{versionText}</p>
         <Button text="check bill" on:click={(z,x,c)=>console.log(z.detail,x,c)} />
         <Button text="check disabled" disabled={btnDisabled} on:click={(z,x,c)=>btnDisabled=true} />
-        <Checkbox bind:value={btnDisabled}>Item 1</Checkbox>
-        <Checkbox text="Item 2" labelBefore />
-      </div>
+        <Button on:click={()=>checkNull=null} >Set null</Button>
+        <Checkbox bind:checked={btnDisabled}>Item 1</Checkbox>
+        <p class="version">Check null value:</p>
+        <Checkbox bind:checked={checkNull} disabled={btnDisabled}>Null value</Checkbox>
+        <p class="version">Check group:</p>
+        <pre>{JSON.stringify(checkOptions)}</pre>
+        <pre>{JSON.stringify(checkResult)}</pre>
+        {#each checkOptions as {id, name}}
+          <Checkbox bind:group={checkResult} value={id} labelBefore>{name}</Checkbox>
+        {/each}
     </Container>
   </div>
 </Viewport>
@@ -45,10 +67,10 @@ h1 {
   color: var(--text-regular-color);
 }
 .aaaa {
-  width: 60%;
-  height: 70%;
-  max-width: 500px;
-  max-height: 400px;
+  width: 70%;
+  height: 80%;
+  max-width: 600px;
+  max-height: 500px;
   margin: auto;
 }
 </style>
