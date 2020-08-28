80ff76b220acf5e381981a5d983eb4f052725443
n.ermolenko <n.ermolenko@rt-solar.ru>
2020-08-29 00:19:57 +0300

    fix app

--- src/App.svelte
+++ src/App.svelte
@@ -1,141 +1,129 @@
 <script>
-  import Router from 'svelte-spa-router';
   import Viewport from './components/Viewport.svelte';
   import Container from './components/Container.svelte';
   import Header from './components/Header.svelte';
   import Button from './components/Button.svelte';
   import Checkbox from './components/Checkbox.svelte';
-  import Radio from './components/Radio.svelte';
 
   export let version;
 
-  let htmlFontSize = 16;
-  let btnDisabled = false;
-  let checkNull = null;
-  let checkOptions = [{
-    id: '001',
-    name: 'Value A'
-  },{
-    id: '002',
-    name: 'Value B'
-  }];
-  let checkResult = ['001'];
-  let radioResult = null;
-  let checkOptions2 = {
-    varA: true,
-    varB: false,
-    varC: null
-  };
+  $: svelteVersion = `Svelte: ${version}`;
 
-  $: versionText = `svelte v${version}`;
+  let darkMode = false;
+  let disabled = false;
+  let originHtmlFontSize = null;
+  let htmlFontSize = null;
+
+  $: helloVersion = `Hello ${version}`;
+  $: disableScaleReset = originHtmlFontSize === null || originHtmlFontSize === htmlFontSize;
+  $: disableScaleDown = htmlFontSize !== null && htmlFontSize < 9;
+  $: disableScaleUp = htmlFontSize > 20;
 
   function fontScaleUp() {
-    const tag = document.getElementsByTagName('html');
+    const tag = document.getElementsByTagName('html')[0];
+    const { fontSize } = getComputedStyle(tag);
 
-    htmlFontSize += 1;
-    tag[0].style.setProperty('font-size', `${htmlFontSize}px`);
+    htmlFontSize = parseInt(fontSize) + 1;
+
+    if(originHtmlFontSize === null) {
+      originHtmlFontSize = parseInt(fontSize);
+    }
+
+    tag.style.setProperty('font-size', `${htmlFontSize}px`);
   }
 
   function fontScaleDown() {
-    const tag = document.getElementsByTagName('html');
+    const tag = document.getElementsByTagName('html')[0];
+    const { fontSize } = getComputedStyle(tag);
 
-    if (htmlFontSize === 0) return;
+    htmlFontSize = parseInt(fontSize) - 1;
 
-    htmlFontSize -= 1;
-    tag[0].style.setProperty('font-size', `${htmlFontSize}px`);
+    if(originHtmlFontSize === null) {
+      originHtmlFontSize = parseInt(fontSize);
+    }
+
+    tag.style.setProperty('font-size', `${htmlFontSize}px`);
   }
 
   function fontScaleReset() {
-    const tag = document.getElementsByTagName('html');
+    const tag = document.getElementsByTagName('html')[0];
 
-    htmlFontSize = 16;
-    tag[0].style.setProperty('font-size', `${htmlFontSize}px`);
+    htmlFontSize = originHtmlFontSize;
+
+    tag.style.setProperty('font-size', `${originHtmlFontSize}px`);
   }
-
-
-  // header="Hello Svelte :: {version} :: qweertyyiuo"
 </script>
 
-<Viewport>
-  <div class="aaaa">
-    <Container>
-      <div slot="header">
-        <Header>
-          <span>{versionText}</span>
-          <Button text="info" rippleCenter />
-        </Header>      
-      </div>
+<Viewport {darkMode}>
+  <Container header="Hello header">
+    <div slot="header">
+      <Header>
+        <span>{svelteVersion}</span>
+         <Button text="btn" raised
+                {disabled} />
+        <Checkbox bind:checked={darkMode}
+                  {disabled}>Dark</Checkbox>
+        <Checkbox bind:checked={disabled}>Disabled </Checkbox>
+        <Button text="btn" rippleCenter {disabled} />
+        <Button text="btn" raised secondary
+                {disabled} />
+        <Button text="btn" secondary
+                {disabled} />
+       
+      </Header>      
+    </div>
+    <div slot="body">
+      <span class="regular-text">{helloVersion}</span>
+      <Checkbox bind:checked={darkMode}
+                {disabled}>Dark</Checkbox>
+      <Button text="raised prim" raised
+                {disabled} />
+      <Button text="prim" 
+                {disabled} />
+      <Button text="raised sec" raised secondary
+                {disabled} />
+      <Button text="sec" secondary
+                {disabled} />
+    </div>
+  </Container>
+  <div class="container-wrap">
+    <Container header="Size control">
       <div slot="body">
-        <h1>Hello svelte</h1>
-        <p class="version">{versionText}</p>
-        <Button text="button" 
-                on:click={(z,x,c)=>console.log(z.detail,x,c)} />
-        <Button disabled={btnDisabled} 
-                on:click={(z,x,c)=>btnDisabled=true} >Set disabled</Button>
-        <Button disabled={checkNull === null}
-                on:click={()=>checkNull=null} >Set null</Button>
-        <Checkbox bind:checked={btnDisabled}>Disabled</Checkbox>
-
-        <br>
-        <Button on:click={fontScaleUp} >ScaleUp</Button>
-        <Button on:click={fontScaleReset} >Reset</Button>
-        <Button on:click={fontScaleDown} >ScaleDwn</Button>
-  
-        <p class="version">Check null value:</p>
-        <pre>{JSON.stringify(checkNull)}</pre>
-        <Checkbox disabled={btnDisabled}
-                  bind:checked={checkNull} >Null value</Checkbox>
-        <Checkbox disabled={btnDisabled}>Inline checkbox</Checkbox>
-        {#each checkOptions as {id, name}}
-          <Radio value={id}
-                 disabled={btnDisabled}
-                 bind:group={radioResult}>{name}</Radio>
-        {/each}
-        <p class="version">Check group:</p>
-        <pre>{JSON.stringify(checkOptions)}</pre>
-        <pre>{JSON.stringify(checkResult)}</pre>
-        {#each checkOptions as {id, name}}
-          <div style="padding: .5rem 0;">
-            <Checkbox value={id}
-                      disabled={btnDisabled}
-                      bind:group={checkResult}  
-                      labelBefore>{name}</Checkbox>
-          </div>
-        {/each}
-        <pre>{JSON.stringify(radioResult)}</pre>
-        {#each checkOptions as {id, name}}
-          <div style="padding: .5rem 0;">
-            <Radio value={id}
-                   disabled={btnDisabled}
-                   bind:group={radioResult}>{name}</Radio>
-          </div>
-        {/each}
+        <Button on:click={fontScaleUp}
+                disabled={disableScaleUp}
+                raised>ScaleUp</Button>
+        <Button on:click={fontScaleReset} 
+                disabled={disableScaleReset}
+                raised
+                secondary>Reset</Button>
+        <Button on:click={fontScaleDown}
+                disabled={disableScaleDown}
+                raised>ScaleDwn</Button>
+      </div>
     </Container>
   </div>
 </Viewport>
 
 <style>
-h1 {
-  color: purple;
+.regular-text {
+  font-size: 1rem;
 }
-.version {
-  font-size: .8rem;
-  color: var(--text-regular-color);
-}
-.aaaa {
-  width: 70%;
+.container-wrap {
+  flex: .5;
+  /* width: 70%;
   height: 80%;
   max-width: 800px;
   max-height: 800px;
-  margin: auto;
+  margin: 1rem auto; */
 }
 
 @media (max-width: 700px) {
-  .aaaa {
+  .container-wrap {
+    /* padding: 1rem;
     margin: inherit;
     width: 100%;
     height: 100%;
-    max-width: none;
+    max-width: none; */
     max-height: none;
   }
 }
