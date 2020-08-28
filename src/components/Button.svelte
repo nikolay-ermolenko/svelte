4769a476a0b927e097df539818098d1e76676323
n.ermolenko <n.ermolenko@rt-solar.ru>
2020-08-29 00:09:46 +0300

    fix button styles

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -8,6 +8,8 @@
   export let rippleCenter = false;
   export let disabled = false;
   export let text = '';
+  export let raised = false;
+  export let secondary = false;
 
   function handleClick(e) {    
     dispatch('click', { disabled, text, event:e });
@@ -15,9 +17,15 @@
 
 </script>
 
-<button class="sv-button {ripple ? 'sv-ripple-el' : ''}" {disabled} on:click|stopPropagation={handleClick}>
+<button class="sv-button"
+        class:sv-ripple-el={ripple}
+        class:disabled={disabled}
+        class:raised={raised}
+        class:secondary={secondary}
+        {disabled}
+        on:click|stopPropagation={handleClick}>
   {#if ripple}
-    <Ripple disabled={disabled} center={rippleCenter} />
+    <Ripple {disabled} center={rippleCenter} />
   {/if}
   <div class="sv-button-content">
     <slot>
@@ -33,34 +41,36 @@
 .sv-button {
   border: none;
   white-space: nowrap;
-  background-color: var(--button-regular-background-color);
+  background-color: var(--button-color);
+  color: var(--button-text-color);
   border-radius: .15rem;
   outline: none;
   margin: .5rem;
   position: relative;
   overflow: hidden;
+  font-size: 1rem;
   transition: .4s cubic-bezier(.4,0,.2,1);
   box-shadow: 0 .3rem .1rem -.2rem var(--shadow-color), 
     0 .2rem .2rem 0 var(--shadow-color), 
     0 .1rem .5rem 0 var(--shadow-color);
 }
-.sv-button:not([disabled]):hover {
-  background-color: #eee;
+.sv-button:not(.disabled):hover {
+  /* background-color: #eee; */
 }
-.sv-button:not([disabled]) {
+.sv-button:not(.disabled) {
   cursor: pointer;
 }
-.sv-button[disabled] {
-  background-color: #f3f3f3;
+.sv-button.disabled {
+  opacity: .3;
   box-shadow: none;
 }
-.sv-button[disabled] .sv-button-content {
-  opacity: .4;
+.sv-button:not(.raised) {
+  box-shadow: none;
+  background-color: transparent;
 }
-
 .sv-button .sv-button-content {
   font-weight: 500;
-  margin: .5rem;
+  margin: .5em;
   font-size: 1rem;
   text-transform: uppercase;
 }
